9425cf87fbe7a4fd8ba6a26df449c92f9bb031e8
Alberto Sánchez <albersanchez511@hotmail.com>
2024-10-24 13:58:37 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className='grid-rows-[auto,1fr,auto grid min-h-screen'>
+        <div className='bg-red-500'>Hola</div>
+        <div className='bg-blue-500'></div>
+        <div className='bg-green-500'></div>
+      </div>
+    </>
+  )
+}
+
+export default App
